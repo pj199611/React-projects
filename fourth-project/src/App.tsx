be966ea9a9e5d719ff31ec2b792d240c86ab08d3
prabhjot_singh <sprabhjot768@gmail.com>
2023-12-12 23:29:28 +0530

    completed mobile design

--- fourth-project/src/App.tsx
+++ fourth-project/src/App.tsx
@@ -1,36 +1,60 @@
 import logo from "/logo.svg";
 import Search from "./components/Search";
 import Button from "./components/Button";
-import { useEffect } from "react";
+import Card from "./components/Card";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const buttonsText = ["All", "Breakfast", "Lunch", "Dinner"];
+
+interface IData {
+  name: String;
+  price: number;
+  text: String;
+  image: String;
+  type: String;
+}
 export default function App() {
   function buttonsClickHandler(text: string) {
     console.log(text);
   }
 
+  const [foodText, setFoodText] = useState<string>();
+  const [cardsData, setCardsData] = useState<IData[]>();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:9000")
+      .then(({ data }) => {
+        setCardsData(data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
   return (
     // adding mobile related css classes
     <div className="h-screen w-screen bg-[#323334] flex flex-col items-center overflow-x-hidden">
       <img src={logo} alt="logo" className="w-[15em] mt-5 flex mx-auto" />
-      <Search />
+      <Search setFoodText={setFoodText} />
 
       <div className="flex flex-row max-w-lg mx-auto space-x-3 mt-10">
         {buttonsText.map((d) => {
           return (
-            <Button
-              buttonsClickHandler={buttonsClickHandler}
-              key={d}
-              text={d}
-            ></Button>
+            <Button buttonsClickHandler={buttonsClickHandler} key={d} text={d}></Button>
           );
         })}
       </div>
-      <div className="bg-[url('../bg.png')] bg-no-repeat bg-cover mt-10 h-screen w-screen flex flex-col px-2 ">
 
-
-
+      {/* Background container with controlled overflow */}
+      <div className="bg-[url('../bg.png')] bg-no-repeat bg-cover mt-10 h-screen w-screen overflow-scroll">
+        <div className="space-y-4 flex flex-col px-6 py-6">
+          {cardsData?.length &&
+            cardsData.map((data,id) => {
+              return <Card key={id} data={data} />;
+            })}
+        </div>
       </div>
     </div>
   );
--- fourth-project/src/components/Card.tsx
+++ fourth-project/src/components/Card.tsx
@@ -1,3 +1,23 @@
-export default function Card() {
-  return <h1>Card</h1>;
+interface IData {
+  name: String;
+  price: number;
+  text: String;
+  image: String;
+  type: String;
+}
+export default function Card({ data }: { data: IData }) {
+  return (
+    <div className="text-white bg-black bg-opacity-50 rounded-2xl flex space-x-2">
+      <div className="w-4/5">
+        <img src={`http://localhost:9000${data.image}`} />
+      </div>
+      <div className="flex flex-col items-start mt-5">
+        <h1>{data.name}</h1>
+        <h2 className="text-lg">{data.text}</h2>
+        <h3 className="mt-6 mb-4 mr-4 ml-auto bg-[#FF4343] px-4 py-1 rounded-md font-bold">
+          ${data.price}
+        </h3>
+      </div>
+    </div>
+  );
 }
