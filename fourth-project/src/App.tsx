fde55a0ccf9eb173398ac2aa91ae498e56841a43
prabhjot_singh <sprabhjot768@gmail.com>
2023-12-12 23:50:29 +0530

    completed mobile view

--- fourth-project/src/App.tsx
+++ fourth-project/src/App.tsx
@@ -2,7 +2,7 @@ import logo from "/logo.svg";
 import Search from "./components/Search";
 import Button from "./components/Button";
 import Card from "./components/Card";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import axios from "axios";
 
 const buttonsText = ["All", "Breakfast", "Lunch", "Dinner"];
@@ -14,46 +14,47 @@ interface IData {
   image: String;
   type: String;
 }
-export default function App() {
-  function buttonsClickHandler(text: string) {
-    console.log(text);
-  }
 
-  const [foodText, setFoodText] = useState<string>();
-  const [cardsData, setCardsData] = useState<IData[]>();
+export default function App() {
+  const [filterText, setFilterText] = useState("");
+  const [cardsData, setCardsData] = useState<IData[]>([]);
 
   useEffect(() => {
     axios
       .get("http://localhost:9000")
-      .then(({ data }) => {
-        setCardsData(data);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
+      .then(({ data }) => setCardsData(data))
+      .catch((e) => console.error(e));
   }, []);
 
+  const handleButtonClick = useCallback((text: string) => {
+    setFilterText(text === "All" ? "" : text);
+  }, []);
+
+  const filteredCards = cardsData.filter((data) => {
+    if (!filterText) return true; // Show all if filterText is empty or "All"
+    const lowerCaseFilter = filterText.toLowerCase();
+    return (
+      data.name.toLowerCase().includes(lowerCaseFilter) ||
+      data.type.toLowerCase().includes(lowerCaseFilter)
+    );
+  });
+
   return (
-    // adding mobile related css classes
     <div className="h-screen w-screen bg-[#323334] flex flex-col items-center overflow-x-hidden">
       <img src={logo} alt="logo" className="w-[15em] mt-5 flex mx-auto" />
-      <Search setFoodText={setFoodText} />
+      <Search setFoodText={(text) => setFilterText(text)} />
 
       <div className="flex flex-row max-w-lg mx-auto space-x-3 mt-10">
-        {buttonsText.map((d) => {
-          return (
-            <Button buttonsClickHandler={buttonsClickHandler} key={d} text={d}></Button>
-          );
-        })}
+        {buttonsText.map((d) => (
+          <Button buttonsClickHandler={handleButtonClick} key={d} text={d} />
+        ))}
       </div>
 
-      {/* Background container with controlled overflow */}
       <div className="bg-[url('../bg.png')] bg-no-repeat bg-cover mt-10 h-screen w-screen overflow-scroll">
         <div className="space-y-4 flex flex-col px-6 py-6">
-          {cardsData?.length &&
-            cardsData.map((data,id) => {
-              return <Card key={id} data={data} />;
-            })}
+          {filteredCards.map((data, id) => (
+            <Card key={`${data.name}-${id}`} data={data} />
+          ))}
         </div>
       </div>
     </div>
